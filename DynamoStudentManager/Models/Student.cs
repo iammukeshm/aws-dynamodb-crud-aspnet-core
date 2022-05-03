f7fbc999870176127dea5e44d26356d0adbfd3d6
Mukesh Murugan <iammukeshm@gmail.com>
2022-05-03 23:04:14 +0530

    Add project files.

---
+++ DynamoStudentManager/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace DynamoStudentManager.Models
+{
+    [DynamoDBTable("students")]
+    public class Student
+    {
+        [DynamoDBHashKey("id")]
+        public int? Id { get; set; }
+
+        [DynamoDBProperty("first_name")]
+        public string? FirstName { get; set; }
+
+        [DynamoDBProperty("last_name")]
+        public string? LastName { get; set; }
+
+        [DynamoDBProperty("class")]
+        public int Class { get; set; }
+
+        [DynamoDBProperty("country")]
+        public string? Country { get; set; }
+    }
+}
