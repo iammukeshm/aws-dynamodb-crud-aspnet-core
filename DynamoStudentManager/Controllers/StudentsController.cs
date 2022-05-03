9e3b7c6d37b7390e68103c69cc84ca214f87a2e2
Mukesh Murugan <iammukeshm@gmail.com>
2022-05-04 01:59:18 +0530

    update

--- DynamoStudentManager/Controllers/StudentsController.cs
+++ DynamoStudentManager/Controllers/StudentsController.cs
@@ -4,59 +4,58 @@ using DynamoStudentManager.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DynamoStudentManager.Controllers
+namespace DynamoStudentManager.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StudentsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class StudentsController : ControllerBase
+    private readonly IDynamoDBContext _context;
+
+    public StudentsController(IDynamoDBContext context)
     {
-        private readonly IDynamoDBContext _context;
+        _context = context;
+    }
 
-        public StudentsController(IDynamoDBContext context)
-        {
-            _context = context;
-        }
+    [HttpGet("{studentId}")]
+    public async Task<IActionResult> GetById(int studentId)
+    {
+        var student = await _context.LoadAsync<Student>(studentId);
+        if (student == null) return NotFound();
+        return Ok(student);
+    }
 
-        [HttpGet("{studentId}")]
-        public async Task<IActionResult> GetById(int studentId)
-        {
-            var student = await _context.LoadAsync<Student>(studentId);
-            if (student == null) return NotFound();
-            return Ok(student);
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetAllStudents()
+    {
+        var student = await _context.ScanAsync<Student>(default).GetRemainingAsync();
+        return Ok(student);
+    }
 
-        [HttpGet]
-        public async Task<IActionResult> GetAllStudents()
-        {
-            var student = await _context.ScanAsync<Student>(default).GetRemainingAsync();
-            return Ok(student);
-        }
+    [HttpPost]
+    public async Task<IActionResult> CreateStudent(Student studentRequest)
+    {
+        var student = await _context.LoadAsync<Student>(studentRequest.Id);
+        if (student != null) return BadRequest($"Student with Id {studentRequest.Id} Already Exists");
+        await _context.SaveAsync(studentRequest);
+        return Ok(studentRequest);
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> CreateStudent(Student studentRequest)
-        {
-            var student = await _context.LoadAsync<Student>(studentRequest.Id);
-            if (student != null) return BadRequest($"Student with Id {studentRequest.Id} Already Exists");
-            await _context.SaveAsync(studentRequest);
-            return Ok(studentRequest);
-        }
+    [HttpDelete("{studentId}")]
+    public async Task<IActionResult> DeleteStudent(int studentId)
+    {
+        var student = await _context.LoadAsync<Student>(studentId);
+        if (student == null) return NotFound();
+        await _context.DeleteAsync(student);
+        return NoContent();
+    }
 
-        [HttpDelete("{studentId}")]
-        public async Task<IActionResult> DeleteStudent(int studentId)
-        {
-            var student = await _context.LoadAsync<Student>(studentId);
-            if (student == null) return NotFound();
-            await _context.DeleteAsync(student);
-            return NoContent();
-        }
-
-        [HttpPut]
-        public async Task<IActionResult> UpdateStudent(Student studentRequest)
-        {
-            var student = await _context.LoadAsync<Student>(studentRequest.Id);
-            if (student == null) return NotFound();
-            await _context.SaveAsync(studentRequest);
-            return Ok(studentRequest);
-        }
+    [HttpPut]
+    public async Task<IActionResult> UpdateStudent(Student studentRequest)
+    {
+        var student = await _context.LoadAsync<Student>(studentRequest.Id);
+        if (student == null) return NotFound();
+        await _context.SaveAsync(studentRequest);
+        return Ok(studentRequest);
     }
 }
